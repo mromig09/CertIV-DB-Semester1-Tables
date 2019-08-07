74706f1cc8630b318208e1c17bad7e667bfee5e8
mromig09 <mromig09@users.noreply.github.com>
2019-08-07 12:23:46 +1000

    Update week_2_tasks.sql

--- week_2_tasks.sql
+++ week_2_tasks.sql
@@ -1,8 +1,8 @@
-drop table if exists playerRegistration;
-drop table if exists teamEntry;
-drop table if exists player;
-drop table if exists season;
-drop table if exists baseballClub;
+drop table playerRegistration;
+drop table teamEntry;
+drop table player;
+drop table season;
+drop table baseballClub;
 
 create table baseballClub (
     clubName        nvarchar(100),
@@ -49,17 +49,22 @@ create table playerRegistration (
 );
 
 insert into baseballClub (clubName, contactName) values
-()
+('Mt Martha Baseball Club', 'Bob Jane');
 
 insert into season (seasonYear, seasonName) values
-()
+(2018, 'Winter'), (2018, 'Summer');
 
 insert into player (playerID, firstName, lastName, phone) values
-()
+(10001, 'Mark', 'Romig', 5551234), (10002, 'John', 'Howard', 5552345),
+(10003, 'Julia', 'Gillard', 5553456);
 
 insert into teamEntry (ageGroup, teamNumber, clubName, seasonYear, seasonName) values
-()
+('Seniors', 1, 'Mt Martha Baseball Club', 2018, 'Winter'),
+('U14', 2, 'Mt Martha Baseball Club', 2018, 'Summer'),
+('U14', 2, 'Mt Martha Baseball Club', 2018, 'Summer');
 
 insert into playerRegistration (dateRegistered, playerID, clubName, seasonYear, seasonName,
             ageGroup, teamNumber) values
-()
+(13-04-2018, 10001, 'Mt Martha Baseball Club', 2018, 'Winter', 'Seniors', 1),
+(22-12-2017, 10002, 'Mt Martha Baseball Club', 2018, 'Summer', 'U14', 2),
+(20-12-2018, 10003, 'Mt Martha Baseball Club', 2018, 'Summer', 'U14', 2);
